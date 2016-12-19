a5ee2c9c8c3ad078c971e0820c2b6071bb574a06
Neha Sah - Vendor <vn0jqzc@m-C02QJ53WG8WM.local>
2016-12-18 23:59:40 -0800

    Added changes to IpatronDao

--- src/main/java/com/cmpe275/dao/IPatronDoa.java
+++ src/main/java/com/cmpe275/dao/IPatronDoa.java
@@ -9,20 +9,45 @@ public interface IPatronDoa {
 
 	public int addPatron(Patron patron);
 	
+	public Book searchBookById (String id);
+	
+	public int checkPatron(String emailid, String password);
+	
+	public int checkActivated(int activated);
+	
+	public void updateActivated( Patron patron);
+	
+	public int checkForSJSUID(int sjsuid);
+	
+	public void addToCart(String name,int flag,int id);
+	
+	public void RemoveFromCart(String name,int flag,int id);
+	
+	public void checkoutBook(String s);
+	
+	public List<Book> searchBook (String name);
+	
+	public List<Book> searchBookbyISBN (String name);
+	
 
-	public Book searchBook (String name);
+	public List<Book> returnBook(int userid,String[] books);
 	
+	public int renewBook(String ISBN, int userid);
 	
-	public int borrowBooks(List<Book> books);
+	public void addWaitlist(String isbn,int userid);
 	
+    public WaitingList2 addTimeWaitlist(int waitid);
+	
+	public void RemoveFromWaitlist(int waitid);
 
-	public int returnBook(Book book,int userid);
-	
-	public int renewBook(Book book, int userid);
-	
-	public int addWaitlist(Book book,int userid);
+	public List<WaitingList2> getWaitinglist(String isbn);
 	
 	public Patron getPatron(int id);
+	
+	
+	public List<Patron> getAllPatrons();
+	
+	
 	
 
 }
